4a5b4659d58099eca5f47a8734f940099da6a80a
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-01-07 19:44:15 -0300

    criado controler, entrega primeiro endpoint

---
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -0,0 +1,44 @@
+package com.eventostec.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.service.EventService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+    
+    @Autowired
+    private EventService eventService;
+    @PostMapping("consumes= multipart/form-data")
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+    public ResponseEntity<Event> create(@RequestParam("title") String title,
+                                        @RequestParam(value = "description", required = false) String descrition,
+                                        @RequestParam("date") Long date,
+                                        @RequestParam("city") String city, 
+                                        @RequestParam("state") String state,
+                                        @RequestParam("remote") Boolean remote,
+                                        @RequestParam("evenUrl") String eventUrl,
+                                        @RequestParam(value = "image", required = false) MultipartFile image) {
+        EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, state, remote eventUrl, image)
+        Event newEvent = this.eventService.createEvent(eventRequestDTO);
+        return ResponseEntity.ok(newEvent);
+    }
+
+}
